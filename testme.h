df9aec72b0935f8435686d0017de15c081d1373d
Zach Byrne <zacharyabyrne@gmail.com>
2015-03-07 16:43:43 -0700

    Add assert int eq macro.

--- testme.h
+++ testme.h
@@ -2,6 +2,7 @@
 #define TEST_ME
 
 #include <stdint.h>
+#include <stdio.h>
 
 #define TESTME_START(name) \
     uint8_t name(void)     \
@@ -9,5 +10,12 @@
     uint8_t result = 0;
 
 #define TESTME_END return result;}
+
+#define TESTME_ASSERT_INT_EQ(a, b) \
+    if(a != b) \
+    { \
+    printf("TESTME Assertion Failed: %d != %d\n", a, b); \
+    return 1; \
+    }
 
 #endif /*TEST_ME*/
